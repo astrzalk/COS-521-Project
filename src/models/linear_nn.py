9fce6d0d9314d28518c207515710f76c896f50c4
Alexander Strzalkowski <alexander.strzalkowski@gmail.com>
2019-01-09 03:38:18 -0500

    Refactors a little and adds eight layer

--- src/models/linear_nn.py
+++ src/models/linear_nn.py
@@ -3,6 +3,7 @@
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 class three_layer_nn(nn.Module):
     """
@@ -12,12 +13,11 @@ class three_layer_nn(nn.Module):
     Instead of outputing
     """
 
-    def __init__(self, init_type='normal', std=1, do_dropout=False, p=0.5, seed=521):
+    def __init__(self, init_type='normal', std=1, do_dropout=False, p=0.5):
         super(three_layer_nn, self).__init__()
-        torch.manual_seed(seed)
         self.mu = 0
         self.s = std
-        self.do_dropout = do_dropout
+        self.do_drop = do_dropout
         if init_type == 'normal':
             self.layer_1 = nn.Linear(128, 32)
             self.layer_2 = nn.Linear(32, 32)
@@ -27,7 +27,7 @@ class three_layer_nn(nn.Module):
             torch.nn.init.normal_(self.layer_3.weight, self.mu, self.s)
         elif init_type == 'balanced':
             raise NotImplementedError
-        if self.do_dropout:
+        if self.do_drop:
             self.p = p
             self.drop = nn.Dropout(self.p)
 
@@ -35,7 +35,7 @@ class three_layer_nn(nn.Module):
         W_1 = self.layer_1.weight
         W_2 = self.layer_2.weight
         W_3 = self.layer_3.weight
-        if self.do_dropout:
+        if self.do_drop:
             W = self.drop(W_3) @ self.drop(W_2) @ self.drop(W_1) # only works in python 3.5+, @ = mat mul
         else:
             W = W_3 @ W_2 @ W_1 # only works in python 3.5+, @ = mat mul
@@ -44,23 +44,53 @@ class three_layer_nn(nn.Module):
 
 class eight_layer_nn(nn.Module):
     """
-    A three layer linear nn with input, hidden and output dims
+    An eight layer linear nn with input, hidden and output dims
     as specified in the paper 'A Convergence Analysis of Gradient Descent
     for Deep Linear Neural Networks'.
     Instead of outputing
     """
 
-    def __init__(self):
+    def __init__(self, init_type='normal', std=1, do_dropout=False, p=0.5):
         super(eight_layer_nn, self).__init__()
-        self.layer_1 = nn.Linear(128, 32)
-        self.layer_2 = nn.Linear(32, 32)
-        self.layer_3 = nn.Linear(32, 1)
+        self.s = std
+        self.do_drop = do_dropout
+        if init_type == 'normal':
+            self.layer_1 = nn.Linear(128, 32)
+            self.layer_2 = nn.Linear(32, 32)
+            self.layer_3 = nn.Linear(32, 32)
+            self.layer_4 = nn.Linear(32, 32)
+            self.layer_5 = nn.Linear(32, 32)
+            self.layer_6 = nn.Linear(32, 32)
+            self.layer_7 = nn.Linear(32, 32)
+            self.layer_8 = nn.Linear(32, 1)
+            torch.nn.init.normal_(self.layer_1.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_2.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_3.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_4.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_5.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_6.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_7.weight, 0, self.s)
+            torch.nn.init.normal_(self.layer_8.weight, 0, self.s)
+        elif init_type == 'balanced':
+            raise NotImplementedError
+
+        if self.do_drop:
+            self.p = p
+            self.drop = nn.Dropout(self.p)
 
     def forward(self):
-        W_1 = self.layer_1.weight
-        W_2 = self.layer_2.weight
-        W_3 = self.layer_3.weight
-        W = W_3 @ W_2 @ W_1 # only works in python 3.5+, @ = mat mul
+        if self.do_drop:
+            W_1, W_2 = self.drop(self.layer_1.weight), self.drop(self.layer_2.weight)
+            W_3, W_4 = self.drop(self.layer_3.weight), self.drop(self.layer_4.weight)
+            W_5, W_6 = self.drop(self.layer_5.weight), self.drop(self.layer_6.weight)
+            W_7, W_8 = self.drop(self.layer_7.weight), self.drop(self.layer_8.weight)
+            W = W_8 @ W_7 @ W_6 @ W_5 @ W_4 @ W_3 @ W_2 @ W_1 # only works in python 3.5+, @ = mat mul
+        else:
+            W_1, W_2 = self.layer_1.weight, self.layer_2.weight
+            W_3, W_4 = self.layer_3.weight, self.layer_4.weight
+            W_5, W_6 = self.layer_5.weight, self.layer_6.weight
+            W_7, W_8 = self.layer_7.weight, self.layer_8.weight
+            W = W_8 @ W_7 @ W_6 @ W_5 @ W_4 @ W_3 @ W_2 @ W_1
         return W
 
 class fro_loss(nn.Module):
@@ -78,30 +108,49 @@ class fro_loss(nn.Module):
         loss = 0.5 * torch.norm(W - cross_cov).pow(2) # Since output_dim is 1 fro norm becomes l2-norm
         return loss
 
-# # Taken from https://github.com/kevinzakka/pytorch-goodies
-# def max_norm(model, max_val=3, eps=1e-8):
-#     for name, param in model.named_parameters():
-#         if 'bias' not in name:
-#             norm = param.norm(2, dim=0, keepdim=True)
-#             desired = torch.clamp(norm, 0, max_val)
-#             param = param * (desired / (eps + norm))
+def train(model, loss_fn, X, y, learning_rate, eps, global_opt, verbose=False):
+    """
+    model         : Type = torch.nn.Module; the neural net model already initialized
+    loss_fn       : Type = torch.nn.Module (technically); just the loss function
+    X             : Type = pytorch tensor; Dataset
+    y             : Type = pytorch tensor; labels
+    learning_rate : Type = float; learning rate for gradient descent
+    eps           : Type = float; tolerance for how close loss is to global_opt
+    global_opt    : Type = float; the global optimum for this problem
+    verbose       : Type = Boolean; If true prints out each iteration, loss pair
+    """
+    loss = np.inf
+    num_iter = 0
+    while torch.abs(global_opt - loss) > eps: # I THINK THIS IS THE BUG
+        W = model() # W_N * W_{N - 1} * ... * W_1
 
-# Their paper used a 3 layer neural network (2 hidden layers) each
-# with dimension 32 each
-# D_in = 128
-# H = 32
-# D_out = 1
+        # Compute and print loss. We pass Tensors containing the predicted and true
+        # values of y, and the loss function returns a Tensor containing the loss.
+        loss = loss_fn(W, X, y)
+        if verbose:
+            print(num_iter, loss.item())
 
-# model = nn.Sequential(
-#         # nn.Dropout(p=0.2), # p = probability of dropping
-#         nn.Linear(D_in, H), # Layer 1 Weights
-#         # nn.Dropout(p=0.2), # p = probability of dropping
-#         nn.Linear(H, H), # Layer 2 Weights
-#         # nn.Dropout(p=0.2), # p = probability of dropping
-#         nn.Linear(H, D_out)  # Layer 3 Weights
-#       )
+        # Zero the gradients before running the backward pass.
+        # In pytorch, gradients are accumulated with .backward(), hence,
+        # we need to zero them out each round
+        model.zero_grad()
 
+        # Backward pass: compute gradient of the loss with respect to all the learnable
+        # parameters of the model. Internally, the parameters of each Module are stored
+        # in Tensors with requires_grad=True, so this call will compute gradients for
+        # all learnable parameters in the model.
+        loss.backward()
 
+        # Update the weights using gradient descent. Each parameter is a Tensor, so
+        # we can access its gradients like we did before.
+        with torch.no_grad():
+            for param in model.parameters():
+                if param.grad is None:
+                    continue
+                param.data -= learning_rate * param.grad
 
-
+        num_iter += 1
+        if num_iter > 1e5: # Breaks training if it takes too long to converge
+            break
+    return num_iter, loss
 
