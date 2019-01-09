f78a54a4ed31a7de5236a525f76041245216868b
Alexander Strzalkowski <alexander.strzalkowski@gmail.com>
2019-01-09 02:25:46 -0500

    Adds dropout and normal init to linear nn

--- src/models/linear_nn.py
+++ src/models/linear_nn.py
@@ -12,8 +12,46 @@ class three_layer_nn(nn.Module):
     Instead of outputing
     """
 
-    def __init__(self):
+    def __init__(self, init_type='normal', std=1, do_dropout=False, p=0.5, seed=521):
         super(three_layer_nn, self).__init__()
+        torch.manual_seed(seed)
+        self.mu = 0
+        self.s = std
+        self.do_dropout = do_dropout
+        if init_type == 'normal':
+            self.layer_1 = nn.Linear(128, 32)
+            self.layer_2 = nn.Linear(32, 32)
+            self.layer_3 = nn.Linear(32, 1)
+            torch.nn.init.normal_(self.layer_1.weight, self.mu, self.s)
+            torch.nn.init.normal_(self.layer_2.weight, self.mu, self.s)
+            torch.nn.init.normal_(self.layer_3.weight, self.mu, self.s)
+        elif init_type == 'balanced':
+            raise NotImplementedError
+        if self.do_dropout:
+            self.p = p
+            self.drop = nn.Dropout(self.p)
+
+    def forward(self):
+        W_1 = self.layer_1.weight
+        W_2 = self.layer_2.weight
+        W_3 = self.layer_3.weight
+        if self.do_dropout:
+            W = self.drop(W_3) @ self.drop(W_2) @ self.drop(W_1) # only works in python 3.5+, @ = mat mul
+        else:
+            W = W_3 @ W_2 @ W_1 # only works in python 3.5+, @ = mat mul
+
+        return W
+
+class eight_layer_nn(nn.Module):
+    """
+    A three layer linear nn with input, hidden and output dims
+    as specified in the paper 'A Convergence Analysis of Gradient Descent
+    for Deep Linear Neural Networks'.
+    Instead of outputing
+    """
+
+    def __init__(self):
+        super(eight_layer_nn, self).__init__()
         self.layer_1 = nn.Linear(128, 32)
         self.layer_2 = nn.Linear(32, 32)
         self.layer_3 = nn.Linear(32, 1)
